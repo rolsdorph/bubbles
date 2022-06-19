112733c18648cc54bf03e16f0e785bf7d5970be5
Mads Rolsdorph <m.rolsdorph@gmail.com>
2022-06-19 20:19:03 +0200

    Add a .env.production helper file to define prod vars
    This should probably become a full-fledged pipeline at some point, but eh, this is enough for now

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,40 +1,48 @@
-const dotenv = require('dotenv').config();
+const dotenv = require('dotenv');
 const path = require('path');
 const webpack = require('webpack');
 
-module.exports = {
-  entry: './src/main.js',
-  output: {
-    path: path.resolve(__dirname, 'out'),
-    filename: 'bundle.js',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      },
-      {
-        test: /\.js$/,
-        include: [
-          path.resolve(__dirname, 'src')
-        ],
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: [
-              '@babel/preset-react'
-            ]
+module.exports = (env) => {
+  dotenv.config({ path: envPath(env) });
+
+  return {
+    entry: './src/main.js',
+    output: {
+      path: path.resolve(__dirname, 'out'),
+      filename: 'bundle.js',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader']
+        },
+        {
+          test: /\.js$/,
+          include: [
+            path.resolve(__dirname, 'src')
+          ],
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                '@babel/preset-react'
+              ]
+            }
           }
         }
-      }
+      ]
+    },
+    plugins: [
+      new webpack.DefinePlugin({
+        DEBUG_MODE: JSON.stringify(process.env['DEBUG_MODE'] === 'true'),
+        WS_URL: JSON.stringify(process.env['WS_URL']),
+        WS_PROTOCOL: JSON.stringify(process.env['WS_PROTOCOL']),
+      })
     ]
-  },
-  plugins: [
-    new webpack.DefinePlugin({
-      DEBUG_MODE: JSON.stringify(process.env['DEBUG_MODE'] === 'true'),
-      WS_URL: JSON.stringify(process.env['WS_URL']),
-      WS_PROTOCOL: JSON.stringify(process.env['WS_PROTOCOL']),
-    })
-  ]
+  }
 };
+
+function envPath(env) {
+  return path.resolve(__dirname, (env.production) ? '.env.production' : '.env');
+}
